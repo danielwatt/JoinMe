ae155005ce1c0397e0fbbdf44ab8cd68fd7d4965
danielwatt <daniel.watt@live.ca>
2015-09-20 01:18:51 -0400

    Good

--- app/src/main/java/join/me/joinme/LoginActivity.java
+++ app/src/main/java/join/me/joinme/LoginActivity.java
@@ -262,7 +262,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void showHashKey() {
